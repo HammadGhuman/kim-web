43015158a8db64744eef169069da3629d4b96da6
Hammad Ghuman <k201720@nu.edu.pk>
2023-09-21 03:40:03 +0500

    remove indenter files

--- components/HowItWork.tsx
+++ components/HowItWork.tsx
@@ -1,31 +1,22 @@
 import React from "react";
+import MobileCard from "./MobileCard";
+const num = [1, 2, 3, 4];
 
 function HowItWork() {
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       <div className="text-center">
         <span className="text-white text-[40px] font-medium leading-[72px]">
-          HOW IT{" "}
+          HOW IT
         </span>
         <span className="text-yellow-600 text-[40px] font-medium leading-[72px]">
           WORKS
         </span>
-      </div>{" "}
-      <div className="mt-5 flex flex-col items-center justify-center -space-y-5">
-        <div className="text-gray-400 text-[22px] font-medium  leading-[72px]">
-          Step #1
-        </div>
-        <div className="text-white text-2xl font-medium  leading-[72px]">
-          Rapportaufbau:
-        </div>
-
-       
       </div>
 
-      <div className="w-[306px] h-[68px] text-white text-sm font-medium ">
-          - Vertrauensaufbau
-          <br />- Verbesserung des Mental Wellbeing
-        </div>
+      {num.map((item) => (
+        <MobileCard key={item} />
+      ))}
     </div>
   );
 }
