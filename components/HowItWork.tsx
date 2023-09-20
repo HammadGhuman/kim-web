2de247d2b1fac2f9823eeebc1f728c7dc8c5f47e
Hammad Ghuman <k201720@nu.edu.pk>
2023-09-21 03:07:06 +0500

    init

---
+++ components/HowItWork.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function HowItWork() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-center">
+        <span className="text-white text-[40px] font-medium leading-[72px]">
+          HOW IT{" "}
+        </span>
+        <span className="text-yellow-600 text-[40px] font-medium leading-[72px]">
+          WORKS
+        </span>
+      </div>{" "}
+      <div className="mt-8 flex flex-col items-center justify-center -space-y-5">
+        <div className="text-gray-400 text-[22px] font-medium  leading-[72px]">
+          Step #1
+        </div>
+        <div className="text-white text-2xl font-medium  leading-[72px]">
+          Rapportaufbau:
+        </div>
+
+       
+      </div>
+
+      <div className="w-[306px] h-[68px] text-white text-sm font-medium ">
+          - Vertrauensaufbau
+          <br />- Verbesserung des Mental Wellbeing
+        </div>
+    </div>
+  );
+}
+
+export default HowItWork;
