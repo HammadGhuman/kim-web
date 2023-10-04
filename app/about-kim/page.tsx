395d29120ea89ccec5050b4801a736ecd99faded
SubhanKhurshid <105592966+SubhanKhurshid@users.noreply.github.com>
2023-10-05 03:45:38 +0500

    half about kim

---
+++ app/about-kim/page.tsx
@@ -0,0 +1,192 @@
+import AboutBackground from "@/components/AboutBackground";
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-[225px] text-center">
+        <span className="text-white text-5xl font-medium font-marsden leading-[72px]">
+          KI.m für{" "}
+        </span>
+        <span className="text-orange-400 text-5xl font-medium font-marsden leading-[72px]">
+          Unterneh{" "}
+        </span>
+        <span className="text-orange-400 text-5xl font-medium font-marsden leading-[72px]">
+          men
+        </span>
+      </div>
+      <div className="w-[291px] text-center text-white text-[13px] font-normal font-marsden">
+        Hier erfährst Du wie KI.m Unternehmen hilft, den Unternehmenserfolg zu
+        steigern.
+      </div>
+
+      <div className="mt-7">
+        <AboutBackground />
+      </div>
+
+      <div className="w-[245px] text-center mt-10">
+        <span className="text-white text-[32px] font-semibold font-marsden">
+          Um erfolgreich zu sein, müssen{" "}
+        </span>
+        <span className="text-orange-400 text-[32px] font-semibold font-marsden">
+          Unternehmen
+        </span>
+      </div>
+      <div className="w-[300px] text-center text-white text-xl font-medium font-marsden mt-5">
+        über Leistungsfähige Führungskräfte verfügen; ihren Mitarbeitern genug
+        Raum für Entwicklung bieten, um für eine hohe Mitarbeiterzufriedenheit &
+        -Bindung zu sorgen; Talente entwickeln & binden; und die Diversität
+        fördern.
+      </div>
+      <div className="mt-20 bg-blue-400 bg-opacity-20 border-2 border-white px-10 py-10 text-center rounded-md">
+        <div className="w-[243px] text-center text-white text-2xl font-medium font-marsden leading-loose">
+          KI.m hilft Unternehmen dabei erfolgreich zu sein, indem KI.m genau
+          diese Aspekte unterstützt! KI.m ist KI-basiertes Mentoring für
+          Führungskräfte. Dabei führt KI.m auf Grundlage einer Conversational AI
+          über eine App auf dem Smartphone einen natürlichen Dialog mit
+          Führungskräften und nutzt unterschiedliche State-of-the-Art
+          Algorithmen, sowie ein wissenschaftlich fundiertes Führungsmodell für
+          das Mentoring.
+        </div>
+      </div>
+
+      <div className="w-[244px] text-center mt-10">
+        <span className="text-white text-5xl font-medium font-marsden leading-[72px]">
+          Vorteile (nutzen)
+        </span>
+        <span className="text-orange-400 text-5xl font-medium font-marsden leading-[72px]">
+          {" "}
+          KI.m
+        </span>
+      </div>
+
+      <div className="w-[237px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-7">
+        Effektivere Führung
+      </div>
+      <div className="w-[318px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 -mt-5">
+        Mit dem Einsatz des Mentoring on the job kann das Portfolio der
+        vorhandenen Führungskräfte besser ausgenutzt werden, da es eine höhere
+        Führungsspanne ermöglicht. Dies wird dadurch erreicht, dass es Führenden
+        erlaubt, ihre Zeit wirksamer und gezielter für Führungsaufgaben
+        einzusetzen.
+      </div>
+      <div className="w-[321px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-48">
+        Mentoring on the job
+      </div>
+      <div className="w-[312px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 -mt-5">
+        Mit dem modernen Ansatz des Mentoring on the job, gehen Unternehmen neue
+        Wege der Führungskräfteentwicklung. Weg von Classroom Learning auf
+        "Vorrat" hin zu einer täglichen Begleitung der Führungskräfte bei der
+        Bewältigung ihrer Aufgaben direkt am Arbeitsplatz.
+      </div>
+
+      <div className="w-[223px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-48">
+        Gesteigerte Mitarbeiterbindung
+      </div>
+      <div className="w-[302px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 mt-4">
+        Mit einem zunehmenden Mangel an qualifizierten Mitarbeitern, besonders
+        im Führungs- und Fachkräftebereich, wird ein
+        Mitarbeiterentwicklungsprogramm zum entscheidenden Erfolgsfaktor für die
+        Mitarbeiterbindung. Dabei müssen budgetschonende Formate zum Einsatz
+        kommen, die es jedem Mitarbeiter und jeder Führungskraft ermöglichen, an
+        diesen teilzunehmen.
+      </div>
+
+      <div className="w-[242px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-72">
+        Wettbewerbsvorteile
+      </div>
+      <div className="w-[264px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 -mt-5">
+        Durch den Einsatz von KI.m können Führungsrisiken gesenkt und Kosten für
+        eine teure Führungskräfteentwicklung eingespart werden.
+      </div>
+
+      <div className="w-[242px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-28">
+        Fördern von Chancengleichheit
+      </div>
+      <div className="w-[264px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 mt-4">
+        Durch flächendeckenden Einsatz von KI.m lernen Führungskräfte einen
+        wertschätzenden, bewussten und respektvollen Umgang mit Verschiedenheit
+        und Individualität.
+      </div>
+
+      <div className="w-[242px] h-[66px] text-center text-white text-2xl font-semibold font-marsden mt-36">
+        Change begleiten
+      </div>
+
+      <div className="w-[304px] h-[83px] text-center text-white text-base font-medium font-marsden leading-7 -mt-4">
+        Die regelmäßige Nutzung von KI.m zur Bewältigung täglicher
+        Führungsherausforderungen unterstützt die Fähigkeit und Bereitschaft
+        Veränderungsmaßnahmen anzunehmen, da über das Mentoring konkrete
+        Lösungen angeboten werden können.
+      </div>
+
+      <div className="w-[304px] text-center mt-44">
+        <span className="text-white text-5xl font-medium font-marsden leading-[72px]">
+          Bedenken der{" "}
+        </span>
+        <span className="text-orange-400 text-5xl font-medium font-marsden leading-[72px]">
+          HR ler
+        </span>
+      </div>
+
+      <div className="mt-20 bg-blue-400 bg-opacity-20 border-2 border-white px-14 py-10 rounded-lg text-center ">
+        <div className="w-[213px] text-center text-orange-400 text-xl font-semibold font-marsden tracking-tight ">
+          Enge Budgets für Führungskräfteentwicklung und mehr Nachfrage als
+        </div>
+        <div className="w-[216px] text-center text-white text-[15px] font-normal font-marsden leading-loose tracking-tight mt-5">
+          Mit KI.m sind enge Budget kein Problem mehr. KI.m bietet ein
+          skalierbares und dadurch kostengünstiges
+          Kompetenzentwicklungsgsprogramm, welches gleichzeitig durch den
+          Einsatz neuster Technologien und KI dazu in der Lage ist wirksam und
+          individuell auf die Belange jedes Teilnehmers einzugehen.
+        </div>
+      </div>
+
+      <div className="mt-10 bg-blue-400 bg-opacity-20 border-2 border-white px-14 py-10 rounded-lg text-center ">
+        <div className="w-[213px] text-center text-orange-400 text-xl font-semibold font-marsden tracking-tight">
+          Oft spiegeln externe Maßnahmen nicht zu unsere individuelle
+          Führungskultur wieder
+        </div>
+        <div className="w-[216px] text-center text-white text-[15px] font-normal font-marsden leading-relaxed tracking-tight mt-5">
+          Das von KI.m angewendete Leadership Model basiert auf Wirkfaktoren,
+          die interkulturelle Gültigkeit haben und empirisch gesichert sind.
+          Somit kann KI.m Führende aus unterschiedlichen Unternehmen, Branchen
+          und mit verschiedenen Hintergründen wirksam begleiten. ABER KI.m kann
+          auch individualisiert und auf die Führungskultur ihres Unternehmens
+          angepasst werden. Kontaktieren sie hierzu unsere Sales Mitarbeiter. –
+          Button Sales!
+        </div>
+      </div>
+
+      <div className="mt-10 bg-blue-400 bg-opacity-20 border-2 border-white px-14 py-10 rounded-lg text-center ">
+        <div className="w-[213px] text-center text-orange-400 text-xl font-semibold font-marsden tracking-tight">
+          Externe Maßnahmen sind im Hinblick auf den Datenschutz oftmals
+          problematisch
+        </div>
+        <div className="w-[216px] text-center text-white text-[15px] font-normal font-marsden leading-7 tracking-tight mt-5">
+          KI.m legt großen Wert auf Datenschutz, daher liegen unsere Server in
+          Deutschland und wir verfolgen von Beginn an einen Privacy-by-Design
+          Ansatz.
+        </div>
+      </div>
+
+      <div className="mt-10 bg-blue-400 bg-opacity-20 border-2 border-white px-6 py-10 rounded-lg text-center ">
+        <div className="w-[335px] text-center">
+          <span className="text-white text-[32px] font-medium font-marsden leading-9">
+            Unser Eigenes{" "}
+          </span>
+          <span className="text-orange-400 text-[32px] font-medium font-marsden leading-9">
+            Sprachmodell
+          </span>
+        </div>
+        <div className="w-[218px] text-center ml-14 text-white text-base font-normal font-marsden leading-[31px] tracking-tight mt-5">
+          Um einen bestmöglichen Schutz ihrer Daten zu garantieren, haben wir
+          unser eigenes KI.m Sprachmodell entwickelt, welches ab Februar 2024
+          für Sie verfügbar sein wird!
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
